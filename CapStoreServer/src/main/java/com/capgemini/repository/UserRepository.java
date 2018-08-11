e280466578ab99940225df96a8610f64b5668e4c
YASHGEEL <yashgeel99@gmail.com>
2018-08-11 15:01:22 +0530

    Delete UserRepository.java

--- CapStoreServer/src/main/java/com/capgemini/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.capgemini.model.Customer;
-import com.capgemini.model.UserRecord;  
-public interface UserRepository extends CrudRepository<Customer, Integer> {  
-}  
